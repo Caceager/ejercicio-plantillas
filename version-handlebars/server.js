addf94fa6ad9d56eb93967f0a2d8852b798b0181
Agustín Céspedes <80867360+Caceager@users.noreply.github.com>
2022-05-25 23:55:36 -0300

    Add files via upload

---
+++ version-handlebars/server.js
@@ -0,0 +1,84 @@
+//jshint esversion:10
+
+
+const express = require('express');
+const { engine } = require('express-handlebars');
+
+
+const app = express();
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+
+
+
+class Product{
+  constructor(title, price, url){
+    this.title = title;
+    this.price = price;
+    this.url = url;
+
+    this.id = undefined;
+  }
+}
+
+class Api{
+  constructor(){
+    this.item_list = [];
+    this.current_index = 0;
+  }
+
+  addItem(item){
+    this.current_index++;
+    item.id = this.current_index;
+    this.item_list.push(item);
+  }
+
+  getAll(){
+    return this.item_list;
+  }
+}
+
+
+
+const api = new Api();
+app.get('/productos', (req, res) =>{
+  res.render('main', {'product': api.item_list});
+});
+
+
+app.post('/productos', (req, res) =>{
+  const title = req.body.title;
+  const price = String(req.body.price);
+  const url = req.body.url;
+
+  if(title.length > 0 && price.length > 0 && url.length > 0){
+    const newProduct = new Product(title, price, url);
+    api.addItem(newProduct);
+    res.redirect('/');
+  }
+  console.log('Se ha agregado un producto.');
+});
+
+
+
+app.engine(
+  "hbs",
+  engine({
+    extname: ".hbs",
+    defaultLayout: 'index.hbs',
+    layoutsDir: './views'
+  })
+);
+
+app.set('view engine', 'hbs');
+app.set('views', './views');
+
+
+
+app.use(express.static('public'));
+
+const port = 8080;
+
+app.listen(port, ()=>{
+  console.log(`Server started on port ${port}`);
+});
